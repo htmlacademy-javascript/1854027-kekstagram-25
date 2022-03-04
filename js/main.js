ce7e75ce39f8048fe74ad2a817ca7d1e6fd2fff8
Romeo <Lakers.08@mail.ru>
2022-03-04 13:37:05 +0300

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function getRandom (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+const checkLine = (row, maxLength) => row.length<maxLength;
