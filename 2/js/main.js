da8346f34cebfcb59eff6b51d14e0a37e140785c
Keks <keks@htmlacademy.ru>
2022-03-05 12:47:29 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,10 +1,7 @@
-export
-
-function getRandom (min, max) {
+export function getRandom (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-export
-const checkLine = (row, maxLength) => row.length<maxLength;
+export const checkLine = (row, maxLength) => row.length<maxLength;
